4af58f96f2070a24de0549dcc90d2a0d2b4e36a6
ventureronin <92182507+ventureronin@users.noreply.github.com>
2023-07-02 16:13:05 -0700

    Revert "some changes"

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,7 +6,7 @@ async def set_default_commands(dp):
         [
             types.BotCommand("start", "Запустить/перезапустить бота"),
             types.BotCommand("help", "Вывести 'помощь':объяснения и обозначения"),
-            types.BotCommand("settings", "Настройки"),
+            types.BotCommand("settings", "Настройки")
             types.BotCommand("location", "Поделиться с ботом своей геолокацией")
         ]
     )
