b3c91ee1c0afe591ccffe3e957db5a9ba311ba0e
Venture_Ronin <venture.ronin@gmail.com>
2023-08-14 12:56:03 +0300

    add tfvars

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,19 +1,3 @@
-variable "GCP_PROJECT_ID" {}
-
-variable "GCP_CREDENTIALS" {}
-
-variable "region_prj" {
-  description = "Region for the project"
-  type        = string
-  default     = "us-central1"
-}
-
-variable "zone_prj" {
-  description = "Zone for the project"
-  type        = string
-  default     = "us-central1-a"
-}
-
 terraform {
   required_providers {
     google = {
