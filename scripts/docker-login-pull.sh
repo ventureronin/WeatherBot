39a50ba95f8dde7087762118a743c472adc2486d
Venture_Ronin <venture.ronin@gmail.com>
2023-08-15 22:41:29 +0300

    fix script

--- scripts/docker-login-pull.sh
+++ scripts/docker-login-pull.sh
@@ -7,14 +7,11 @@ fi
 
 sudo apt update
 sudo apt install -y docker.io
+sudo usermod -aG docker "$USER"
 
-# Замените '<ваше_имя_пользователя>' и '<ваш_пароль>' на свои данные
 USERNAME="$1"
 PASSWORD="$2"
 
-docker login -u "USERNAME" -p "PASSWORD"
-#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin
 
-docker run ventureronin/tg_weatherbot:latest
-#docker run -d <имя_контейнера>
-echo "Script SUCCSED"
+docker run -d ventureronin/tg_weatherbot:latest
