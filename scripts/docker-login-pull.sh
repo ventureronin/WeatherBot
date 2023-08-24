17e77b8eb121d0eb493756b6ed4a9893ffdc36f8
Venture_Ronin <venture.ronin@gmail.com>
2023-08-24 13:06:24 +0300

    fix timezone for container

--- scripts/docker-login-pull.sh
+++ scripts/docker-login-pull.sh
@@ -16,4 +16,4 @@ PASSWORD="$2"
 
 echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin
 docker pull ventureronin/tg_weatherbot:latest
-docker run -d ventureronin/tg_weatherbot:latest
+docker run -d -e TZ=Europe/Kiev ventureronin/tg_weatherbot:latest
