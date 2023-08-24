a8b8cef6cf3c64e41f4a4eca8103017075904c87
Venture_Ronin <venture.ronin@gmail.com>
2023-08-24 13:04:29 +0300

    fix

--- app/app.py
+++ app/app.py
@@ -17,7 +17,7 @@ async def on_startup(dispatcher):
     await set_default_commands(dispatcher)
     asyncio.ensure_future(set_scheduler())
     logs = open('logs.log', 'a', encoding='utf-8')
-    logs.write(f"[{datetime.datetime.now()}]Bot starting... OK")
+    logs.write(f"[{datetime.datetime.now()}]Bot starting... OK\n")
     print("Bot starting... OK🤖")
 
 
